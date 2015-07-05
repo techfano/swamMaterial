7bbf71ad7e1a3668bafa9e8dbbab1dcf7bb3543f
Estefano <mixc21@gmail.com>
2015-07-05 02:52:49 -0500

    design login

--- source/scripts/controller/demo.ctrl.js
+++ source/scripts/controller/demo.ctrl.js
@@ -1,12 +1,12 @@
 (function() {
   'use strict';
   angular.module('module.demo', [])
-    .controller('demo.ctrl', function($scope) {
-      $scope.isOpen = false;
-      $scope.demo = {
-        isOpen: false,
-        count: 0,
-        selectedAlignment: 'md-left'
-      };
-    });
+    .controller('demo.ctrl', demoCtrl); 
+
+    function demoCtrl($scope) {
+      $scope.getSignIn=function(user){
+        console.info(user);
+      }
+    }
+    
 })();
