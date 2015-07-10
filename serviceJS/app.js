5c4518a71c67555bac4d2daf65519548c79c3837
Estefano <mixc21@gmail.com>
2015-07-10 01:16:26 -0500

    changin user data

--- serviceJS/app.js
+++ serviceJS/app.js
@@ -96,14 +96,14 @@ app.get('/api/auth/login/:username/:password', function(req, res) {
 app.post('/api/user/create', function(req, res) {
 
 //	var newUser = user(req.body);
-	console.log(req.body);
+	console.log(req);
 	// if (!req.body){ 
 	// 	return res.sendStatus(400);
 	// }else{
 
 	// 	newUser.save(function(err) {
 	// 	  if (err){ res.send(err); };
-	// 	  res.send('User created!');
+	 	  res.send('User created!');
 	// 	});
 
 	// }
