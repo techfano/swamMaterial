98e57d3cb90c215aca04f0b10414db52af756925
techfano <mixc21@gmail.com>
2015-10-20 12:43:42 -0500

    adding svg icons an fix layout

--- source/scripts/app.js
+++ source/scripts/app.js
@@ -75,12 +75,11 @@
 
         $mdIconProvider
                       .defaultIconSet("svg/avatars.svg", 128)
-                      .icon("menu"       , "svg/menu.svg"        , 24)
-                      .icon("share"      , "svg/share.svg"       , 24)
-                      .icon("google_plus", "svg/google_plus.svg" , 512)
-                      .icon("hangouts"   , "svg/hangouts.svg"    , 512)
-                      .icon("twitter"    , "svg/twitter.svg"     , 512)
-                      .icon("phone"      , "svg/phone.svg"       , 512);
+                      .icon("menu", "svg/menu.svg", 24)
+                      .icon("share", "svg/share.svg", 24)
+                      .icon("arrow_back", "svg/arrow_back.svg", 24)
+                      .icon("arrow_forward", "svg/arrow_forward.svg", 24)
+                      .icon("close", "svg/close.svg", 24);
 
                       $mdThemingProvider.theme('default')
                           .primaryPalette('blue')
